9541cbbf6d2ef0a3f4956652f901dde2d3781ab8
thilakjo <thilaks195@gmail.com>
2025-05-24 17:54:32 +0530

    some api issues with backend

--- backend/main.py
+++ backend/main.py
@@ -383,3 +383,11 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://jo-speaks-frontend.vercel.app"],  # or ["*"] for all origins (not recommended for prod)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
